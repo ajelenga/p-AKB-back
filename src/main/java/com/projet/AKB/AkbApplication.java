fece481bb0c00401662c18afa49537c1e6a0bb2b
ajelenga <elengaalphonse107@gmail.com>
2023-01-05 13:22:37 +0100

    start

---
+++ src/main/java/com/projet/AKB/AkbApplication.java
@@ -0,0 +1,13 @@
+package com.projet.AKB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/AKB/AkbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.AKB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
